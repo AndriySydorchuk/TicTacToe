549578e26677fee8cd833d8e6a4713340250692d
AndriySydorchuk <programmers130822@gmail.com>
2024-03-17 15:05:56 +0200

    Impelement immediately reading input, so the user could navigate the field without hitting Enter every time

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <stdio.h>
+#include <termios.h>
 #include "Field.h"
+
+static struct termios stored_settings;
+
+void set_keypress(void)
+{
+    struct termios new_settings;
+
+    tcgetattr(0,&stored_settings);
+
+    new_settings = stored_settings;
+
+    /* Disable canonical mode, and set buffer size to 1 byte */
+    new_settings.c_lflag &= (~ICANON);
+    new_settings.c_cc[VTIME] = 0;
+    new_settings.c_cc[VMIN] = 1;
+
+    tcsetattr(0,TCSANOW,&new_settings);
+    return;
+}
+
+void reset_keypress(void)
+{
+    tcsetattr(0,TCSANOW,&stored_settings);
+    return;
+}
 
 int main()
 {
-    std::cout << "\n\n";
+    set_keypress();
 
     Field field;
     field.Show();
 
+    int key = getchar();
+    std::cout << key << std::endl;
+
+    reset_keypress();
+
+    std::cin.ignore();
     std::cin.get();
     return 0;
 }
