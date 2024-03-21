d12a37b23e11ebb62682fd46721cc2c4f0909b49
AndriySydorchuk <programmers130822@gmail.com>
2024-03-21 09:40:02 +0200

    Create separate Tools namespace for helper functions for console, cursor etc

---
+++ Tools.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <termios.h>
+
+namespace Tools
+{
+    void SetCursorVisibility(bool on_or_off)
+    {
+        if(on_or_off == false)
+        {
+            std::cout << "\e[?25l"; // disable cursor
+        }
+        else
+        {
+            std::cout << "\e[?25h"; // enable cursor
+        }
+    }
+
+    void SetCursorPosition(size_t x, size_t y)
+    {
+        std::string escapeSequence = "\033[" + std::to_string(x) + ";" + std::to_string(y) + "f";
+        std::cout << escapeSequence;
+    }
+
+    static struct termios storedSettings;
+
+    void SetKeypress(void)
+    {
+        struct termios newSettings;
+
+        tcgetattr(0,&storedSettings);
+
+        newSettings = storedSettings;
+
+        /* Disable canonical mode, and set buffer size to 1 byte */
+        newSettings.c_lflag &= (~ICANON);
+        newSettings.c_cc[VTIME] = 0;
+        newSettings.c_cc[VMIN] = 1;
+
+        tcsetattr(0,TCSANOW,&newSettings);
+        return;
+    }
+
+    void ResetKeypress(void)
+    {
+        tcsetattr(0,TCSANOW,&storedSettings);
+        return;
+    }
+}
