63f77b632a693a8bb50e0355135efc5cf9de26e7
AndriySydorchuk <programmers130822@gmail.com>
2024-03-21 10:02:34 +0200

    Add several basic game enums that I need

--- GameEnums.h
+++ GameEnums.h
@@ -1,6 +1,34 @@
 #pragma once
 
-enum Key
+enum class GameWindow
+{
+    StartScreen,
+    Menu,
+    GameScene
+};
+/*
+
+enum class MenuType
+{
+    MainMenu,
+
+};
+*/
+
+enum class Direction
+{
+    Up, Down, Left, Right, None
+};
+
+enum class MainMenuItems
+{
+    StartGame,
+    Options,
+    Exit
+};
+
+
+enum class Key
 {
     ARROW_LEFT = 97, ARROW_UP = 119, ARROW_RIGHT = 100, ARROW_DOWN = 115
 };
